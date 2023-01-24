1728f268be864c4246ef294638e8ab5c9d3d289a
Anyuxuan <xiayu_12@yeah.net>
2023-01-24 17:34:14 +0800

    Update Script-1.sql

--- Script-1.sql
+++ Script-1.sql
@@ -4280,6 +4280,56 @@ SELECT * FROM travel ORDER BY money DESC LIMIT 0,1
 
 SELECT SUM(money) FROM travel WHERE aid = (SELECT id FROM agency WHERE name = '青年旅行社')
 
+SHOW DATABASES;
+
+USE atguigudb;
+
+SELECT DATABASE()
+
+CREATE VIEW employee_vu
+AS SELECT last_name, employee_id,department_id FROM employees e;
+SELECT * FROM employee_vu
+
+DESC employee_vu
+
+CREATE OR REPLACE VIEW employee_vu 
+AS SELECT e.last_name, e.employee_id, e.department_id FROM employees e WHERE e.department_id = 80;
+
+CREATE TABLE emps AS
+SELECT * FROM employees e;
+
+SELECT * FROM employees e;
+
+CREATE VIEW emp_v1 
+AS SELECT last_name, salary,email FROM emps WHERE phone_number LIKE '011%'
+
+SELECT * FROM emp_v1
+
+CREATE OR REPLACE VIEW emp_v1
+AS SELECT last_name, email, phone_number FROM emps WHERE phone_number LIKE '011%' AND email LIKE '%_e%'
+
+DESC emp_v1;
+
+INSERT INTO emp_v1 (last_name, email, phone_number)
+VALUES ('lili', 'xiao@eee', '0111.999990')
+
+UPDATE emp_v1 
+SET salary = salary + 1000
+
+DELETE FROM emp_v1
+WHERE last_name = 'Olsen'
+
+SELECT * FROM emp_v1
+
+SELECT * FROM emps
+
+CREATE VIEW emp_v2
+AS SELECT department_id, MAX(salary) AS max_salary FROM emps GROUP BY department_id HAVING MAX(salary) > 12000 ;
+
+SELECT * FROM emp_v2
+
+DROP VIEW IF EXISTS emp_v1,emp_v2
+
 
 
 
