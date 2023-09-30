b3b464e542e194ec5ce7bc1f07097e7d762fc816
xiayefeng <xiayu_12@yeah.net>
2023-09-30 17:13:01 +0800

    Update Script-1.sql

--- Script-1.sql
+++ Script-1.sql
@@ -4786,8 +4786,57 @@ SHOW DATABASES;
 SELECT database();
 SHOW tables;
 
+CREATE PROCEDURE test_loop()
+BEGIN 
+    DECLARE num int DEFAULT 1;
+    loop_label:LOOP 
+        SET num = num + 1;
+       IF num >= 10 THEN LEAVE loop_label;
+       END IF;
+    END LOOP loop_label;
+    SELECT num FROM DUAL;
+END;
 
+DROP PROCEDURE test_loop;
 
+CALL test_loop();
+
+CREATE PROCEDURE update_salary_loop (OUT num int)
+BEGIN 
+     DECLARE avg_sal double;
+     DECLARE loop_count int DEFAULT 0;
+     SELECT AVG(e.salary) INTO avg_sal FROM employees e;
+     loop_lab: LOOP 
+         IF avg_sal >= 12000
+           THEN LEAVE loop_lab;
+         END IF;
+         UPDATE employees SET salary = salary * 1.1;
+         SELECT AVG(e.salary) INTO avg_sal FROM employees e;
+         SET loop_count = loop_count + 1;
+     END LOOP loop_lab;
+     SET num = loop_count;
+END ;
+
+SELECT AVG(e.salary) FROM employees e;
+
+SET @num = 0;
+
+CALL update_salary_loop(@num);
+
+SELECT @num;
+
+CREATE PROCEDURE test_while()
+BEGIN 
+    DECLARE num int DEFAULT 1;
+    WHILE num <= 10 DO
+      SET num = num + 1;
+     END WHILE;
+    SELECT num;
+END;
+
+DROP PROCEDURE test_while;
+
+CALL test_while();
 
 
 
